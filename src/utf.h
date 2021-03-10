09d9372c00b12f92cacb4183bf2ffd3711f9caeb
js-j <52243635+js-j@users.noreply.github.com>
2021-03-10 15:20:27 +0000

    Update utf.h

--- src/utf.h
+++ src/utf.h
@@ -22,7 +22,8 @@
 
 #include <stdint.h>
 
-uint32_t inline decode(uint32_t *state, uint32_t *codep, uint32_t byte);
+//https://github.com/payden/libwebsock/issues/22
+uint32_t  decode(uint32_t *state, uint32_t *codep, uint32_t byte);
 
 
 #endif /* UTF_H_ */
